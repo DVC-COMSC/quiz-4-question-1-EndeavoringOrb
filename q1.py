873270da1fdb20f40b1bbc709d952518e63eda66
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-01 20:04:46 -0700

    6th commit

--- q1.py
+++ q1.py
@@ -7,12 +7,12 @@ num_arr=[]
 
 
 for i in range(10):
-    num_arr.append(int(input()))
+    num_arr.append(int(input()))
 
 
 for i in range(9):
-    if num_arr[i]%2==0 and num_arr[i+1]%2==0:
-        adjacent += 1
+    if num_arr[i]%2==0 and num_arr[i+1]%2==0:
+        adjacent += 1
 
 
 print(adjacent)
