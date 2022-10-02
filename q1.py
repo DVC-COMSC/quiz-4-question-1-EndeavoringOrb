e28551d6282368508921b6c93fb06745e791bb88
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-01 19:37:32 -0700

    1st commit

--- q1.py
+++ q1.py
@@ -3,6 +3,16 @@
 # Make your Code
 # ******************************
 
+adjacent=0
+num = int(input("Enter number "+str(1)+ ": "))
+
+for i in range(9):
+    t_num = int(input("Enter number "+str(i+2)+ ": "))
+    if num%2 == 0 and t_num%2 == 0:
+        adjacent += 1
+    num = t_num
+
+print(adjacent)
 
 # Requirement
 # No need to use list
