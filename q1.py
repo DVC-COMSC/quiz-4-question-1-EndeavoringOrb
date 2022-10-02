c64062c904f44fbb3daf342d7857b710f1d97c6e
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-01 20:19:57 -0700

    7th commit

--- q1.py
+++ q1.py
@@ -1,8 +1,5 @@
-# ******************************
-# Make your Code
-# ******************************
-
 adjacent=0
+chain = 0
 num_arr=[]
 
 
@@ -12,12 +9,11 @@ for i in range(10):
 
 for i in range(9):
     if num_arr[i]%2==0 and num_arr[i+1]%2==0:
-        adjacent += 1
+        if chain == 0:
+            adjacent += 1
+        chain = 1
+    else:
+        chain = 0
 
-
+print("-----------------")
 print(adjacent)
-
-# Requirement
-# No need to use list
-# All input values are taken one by one separatively.
-###
